a6d78c1aa9cd4bd0838f0ff6d9e91486050fff55
jiocode-edu <ifmdac80@gmail.com>
2025-12-31 17:33:55 +0530

    31-12-2025

---
+++ 31-12/index.js
@@ -0,0 +1,103 @@
+// console.log("Hello Javascript");
+
+// console.warn("warning")
+
+// console.error("error")
+
+// document.write("Hello")
+
+// // DOM
+
+// document.getElementById("demo").innerHTML  = "Happy New Year 2026"
+
+// alert("Happy New Year 2026")
+
+// var num;
+
+// let data = prompt("Enter any number")
+
+// console.log(typeof data);
+
+// console.log(data);
+
+// var keyword
+
+var x = 
+x = 30
+{
+  var x = 10
+}
+
+console.log(x);
+
+
+// 2015 ES6
+
+// let , const
+
+// block scope
+
+{
+  let y = 20
+  y = 30
+  console.log(y);
+}
+
+{
+  const z = 20
+  console.log(z);
+}
+
+// Template Literals (back-tik)
+
+{
+  let toys = "truck"
+
+  let str1 = 'Hello , "javascript" `Hello`'
+  let str2 = "Hello 'javascript ' `Hello`"
+  let str3 = `Hello 
+  'Hello' 
+  "Hello"
+   ${toys}`
+
+  console.log(str1 , str2 , str3);
+}
+
+// DataTypes
+
+
+let num = 10
+let str = "Hello"
+let bool = true
+let symbol = Symbol()
+let Null = null
+let Undefined = undefined
+let bigInt = 100000000n
+let arr1 = [1]
+let arr2 = [1]
+let obj = {}
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof Null);
+console.log(typeof Undefined);
+console.log(typeof bigInt);
+console.log(typeof symbol);
+console.log(typeof arr1);
+console.log(typeof obj);
+
+console.log(arr1 === obj);
+
+console.log(arr1 === arr2);
+
+
+
+
+
+
+
+
+
+
+
